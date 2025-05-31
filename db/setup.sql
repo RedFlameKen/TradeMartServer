af80f1854ebccc8418a16f45713eff44adbf4e3c
redflameken <kabalacaba@gmail.com>
2025-05-31 19:15:03 +0800

    skill_rating and remove auto_increments

--- db/setup.sql
+++ db/setup.sql
@@ -3,7 +3,7 @@ create database if not exists trademart;
 use trademart;
 
 create table if not exists users (
-    user_id int primary key auto_increment,
+    user_id int primary key,
     name varchar(256),
     email varchar(256),
     password varchar(256),
@@ -13,7 +13,7 @@ create table if not exists users (
 );
 
 create table if not exists skill_cards (
-    skill_card_id int primary key auto_increment,
+    skill_card_id int primary key,
     skill_title varchar(256),
     skill_description varchar(1024),
     tags varchar(1024),
@@ -22,8 +22,16 @@ create table if not exists skill_cards (
     foreign key(user_id) references users(user_id)
 );
 
+create table if not exists skill_rating (
+    skill_rating_id int primary key,
+    rating double,
+    comment varchar(1024),
+    rater_id int,
+    foreign key(rater_id) references users(user_id)
+);
+
 create table if not exists text_messages (
-    message_id int primary key auto_increment,
+    message_id int primary key,
     target_user_id int,
     message_text varchar(4096),
     response_text varchar(4096),
@@ -34,7 +42,7 @@ create table if not exists text_messages (
 );
 
 create table if not exists job_postings (
-    job_id int primary key auto_increment,
+    job_id int primary key,
     job_title varchar(255),
     job_type varchar(128),
     job_category varchar(255),
@@ -46,7 +54,7 @@ create table if not exists job_postings (
 );
 
 create table if not exists media (
-    media_id int primary key auto_increment,
+    media_id int primary key,
     short_video_id int,
     media_url varchar(2000),
     date_uploaded datetime,
@@ -55,7 +63,7 @@ create table if not exists media (
 );
 
 create table if not exists report_contents (
-    report_id int primary key auto_increment,
+    report_id int primary key,
     report_type varchar(255)
 );
 
