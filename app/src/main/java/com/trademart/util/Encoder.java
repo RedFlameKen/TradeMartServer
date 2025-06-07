3e2f5c12c6d51c1b3c6f63d964e6758da32d9778
RedFlameKen <nettolacaba@gmail.com>
2025-06-07 23:31:48 +0800

    Some updates on the utils

--- app/src/main/java/com/trademart/util/Encoder.java
+++ app/src/main/java/com/trademart/util/Encoder.java
@@ -12,6 +12,14 @@ public class Encoder {
         return Base64.getEncoder().encodeToString(data);
     }
     
+    public static String encodeURLBase64(String data){
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(data.getBytes());
+    }
+
+    public static String encodeURLBase64(byte[] data){
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(data);
+    }
+
     public static byte[] decodeBase64(String data){
         return Base64.getDecoder().decode(data);
     }
@@ -20,4 +28,11 @@ public class Encoder {
         return new String(Base64.getDecoder().decode(data));
     }
 
+    public static byte[] decodeURLBase64(String data){
+        return Base64.getUrlDecoder().decode(data);
+    }
+
+    public static String decodeURLBase64(byte[] data){
+        return new String(Base64.getUrlDecoder().decode(data));
+    }
 }
--- app/src/main/java/com/trademart/util/FileUtil.java
+++ app/src/main/java/com/trademart/util/FileUtil.java
@@ -4,9 +4,9 @@ public class FileUtil {
 
     public static String getExtension(String filename){
         StringBuilder builder = new StringBuilder();
-        for (int i = filename.length(); i >= 0; i--) {
+        for (int i = filename.length()-1; i >= 0; i--) {
             char curChar = filename.charAt(i);
-            if(curChar != '.'){
+            if(curChar != '.' && curChar != '.'){
                 builder.insert(0, curChar);
                 continue;
             }
