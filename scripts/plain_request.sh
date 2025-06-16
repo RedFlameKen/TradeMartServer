d64f11e1da9c1206143390b540f2a8aa99232119
RedFlameKen <kabalacaba@gmail.com>
2025-06-17 00:30:10 +0800

    not sure what the hell i was doing but committing it anyway

--- scripts/plain_request.sh
+++ scripts/plain_request.sh
@@ -9,4 +9,15 @@ Connection: close\r\n\
 \r\n\
 "
 
-printf "$request" | netcat 127.0.0.1 8080
+video_data=$(cat ~/Videos/memes/fallguys_battlepass.mp4 | base64)
+
+post_video_request="POST /post/publish/95704/media HTTP/1.1\r\n\
+Host: 127.0.0.1\r\n\
+Content-Type: application/json\r\n\
+Connection: close\r\n\
+\r\n\
+{\"filename\":\"fortnite battlepass\",\"data\":\"$video_data\"}\
+\r\n\
+"
+
+printf "$post_video_request" | netcat 127.0.0.1 8080
