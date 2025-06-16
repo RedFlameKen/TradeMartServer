b720071abb34bee9cebc38fea03b7d2f8f8b1b4d
RedFlameKen <kabalacaba@gmail.com>
2025-06-16 22:49:15 +0800

    add removeExtension() FileUtil method and remove the test && condition

--- app/src/main/java/com/trademart/util/FileUtil.java
+++ app/src/main/java/com/trademart/util/FileUtil.java
@@ -6,7 +6,7 @@ public class FileUtil {
         StringBuilder builder = new StringBuilder();
         for (int i = filename.length()-1; i >= 0; i--) {
             char curChar = filename.charAt(i);
-            if(curChar != '.' && curChar != '.'){
+            if(curChar != '.'){
                 builder.insert(0, curChar);
                 continue;
             }
@@ -14,4 +14,20 @@ public class FileUtil {
         }
         return builder.toString();
     }
+
+    public static String removeExtension(String filename){
+        int extension_index = -1;
+        for (int i = filename.length()-1; i >= 0; i--) {
+            char curChar = filename.charAt(i);
+            if(curChar == '.'){
+                extension_index = i;
+                break;
+            }
+        }
+        if(extension_index == -1){
+            return filename;
+        }
+        return filename.substring(0, extension_index);
+    }
+
 }
